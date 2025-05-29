7a78c4818c0797af22dda02bde55d2861ccc467a
DonizeteVida <donizetevida1998@outlook.com>
2025-05-30 00:46:04 +0200

    Add initial chip_8 hardware spec

--- main.c
+++ main.c
@@ -1,6 +1,21 @@
 #include <emulator/lifecycle.h>
 #include <emulator/display.h>
 
+#define CHIP_8_PROGRAM_START 0x200
+#define CHIP_8_WIDTH 64
+#define CHIP_8_HEIGHT 32
+
+struct chip_8_t {
+    uint8_t memory[4096];
+    uint16_t pc;
+    uint16_t stack[16];
+    uint8_t sp;
+    uint8_t rg[16];
+    uint16_t I;
+};
+
+static struct chip_8_t chip_8;
+
 void emulator_lifecycle_init(void **appstate, int argc, char *argv[])
 {
 }
