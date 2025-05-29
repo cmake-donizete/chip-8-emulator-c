e8b8bd47d5e591e514dcf2cbb88e0f56980fe2ed
DonizeteVida <donizetevida1998@outlook.com>
2025-05-30 01:36:55 +0200

    Add initial file read code

--- main.c
+++ main.c
@@ -1,3 +1,8 @@
+#include <stdio.h>
+
+#include <fcntl.h>
+#include <unistd.h>
+
 #include <emulator/lifecycle.h>
 #include <emulator/display.h>
 
@@ -5,7 +10,8 @@
 #define CHIP_8_WIDTH 64
 #define CHIP_8_HEIGHT 32
 
-struct chip_8_t {
+struct chip_8_t
+{
     uint8_t memory[4096];
     uint16_t pc;
     uint16_t stack[16];
@@ -18,6 +24,25 @@ static struct chip_8_t chip_8;
 
 void emulator_lifecycle_init(void **appstate, int argc, char *argv[])
 {
+    int fd;
+    size_t count;
+    uint8_t *buffer = chip_8.memory + CHIP_8_PROGRAM_START;
+
+    fd = open("../rom/ibmlogo.ch8", O_RDONLY);
+    count = read(fd, buffer, sizeof(chip_8.memory) - CHIP_8_PROGRAM_START);
+
+    for (size_t start = 0; start < count; start += 2)
+    {
+        if (start % 0x10 == 0)
+        {
+            printf("\n");
+        }
+        printf("%02x", buffer[start + 1]);
+        printf("%02x", buffer[start]);
+        printf(" ");
+    }
+
+    close(fd);
 }
 
 void emulator_lifecycle_iterate(void *appstate)
