9f935aa5ec9d193268fd6b85053f56396311b52b
DonizeteVida <donizetevida1998@outlook.com>
2025-06-07 18:50:22 +0200

    Add initial opcode decode steps

--- main.c
+++ main.c
@@ -7,7 +7,23 @@
 #include <emulator/lifecycle.h>
 #include <emulator/display.h>
 
-#define CHIP_8_PROGRAM_START 0x200
+#define CHIP_8_MEMORY_SIZE      0x1000
+#define CHIP_8_FONT_START       0x0000
+#define CHIP_8_PROGRAM_START    0x0200
+#define CHIP_8_DISPLAY_START    0x0F00
+
+#define CHIP_8_OPCODE_CLEAR     0x00E0  // 0x00E0
+#define CHIP_8_OPCODE_RET       0x00EE  // 0x00EE
+#define CHIP_8_OPCODE_JUMP      0x1000  // 0x1NNN
+#define CHIP_8_OPCODE_CALL      0x2000  // 0x2NNN
+#define CHIP_8_OPCODE_EQNN      0x3000  // 0x3XNN
+#define CHIP_8_OPCODE_NENN      0x4000  // 0x4XNN
+#define CHIP_8_OPCODE_EQXY      0x5000  // 0x5XN0
+#define CHIP_8_OPCODE_MOVX      0x6000  // 0x6XNN
+#define CHIP_8_OPCODE_INCX      0x7000  // 0x7XNN
+#define CHIP_8_OPCODE_MOVR      0x8000  // 0x8XY0
+#define CHIP_8_OPCODE_MOVI      0xA000  // 0xANNN
+#define CHIP_8_OPCODE_DISP      0xD000  // 0xDXYN
 
 const static uint8_t FONT[] = {
     0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
@@ -30,43 +46,144 @@ const static uint8_t FONT[] = {
 
 struct chip_8_t
 {
-    uint8_t memory[4096];
+    uint8_t memory[CHIP_8_MEMORY_SIZE];
     uint16_t pc;
     uint16_t stack[16];
     uint8_t sp;
     uint8_t rg[16];
     uint16_t I;
+    uint8_t *program;
+    uint8_t *display;
+    uint8_t *font;
 };
 
 static struct chip_8_t chip_8;
 
+static inline void chip_8_memory_init()
+{
+    memset(&chip_8, 0, sizeof(chip_8));
+
+    chip_8.pc = CHIP_8_PROGRAM_START;
+    chip_8.program = chip_8.memory + CHIP_8_PROGRAM_START;
+    chip_8.display = chip_8.memory + CHIP_8_DISPLAY_START;
+    chip_8.font = chip_8.memory + CHIP_8_FONT_START;
+
+    memcpy(chip_8.font, FONT, sizeof(FONT));
+}
+
 void emulator_lifecycle_init(void **appstate, int argc, char *argv[])
 {
+    chip_8_memory_init();
+
     int fd;
-    size_t count;
-    uint8_t *buffer = chip_8.memory + CHIP_8_PROGRAM_START;
+    size_t readed;
 
     fd = open("../rom/ibmlogo.ch8", O_RDONLY);
-    count = read(fd, buffer, sizeof(chip_8.memory) - CHIP_8_PROGRAM_START);
+    readed = read(fd, chip_8.program, CHIP_8_MEMORY_SIZE - CHIP_8_PROGRAM_START);
 
-    for (size_t start = 0; start < count; start += 2)
+    for (size_t offset = 0; offset < readed; offset += 2)
     {
-        if (start % 0x10 == 0)
+        if (offset % 0x10 == 0)
         {
             printf("\n");
         }
-        printf("%02x", buffer[start + 1]);
-        printf("%02x", buffer[start]);
+        printf("%02x", chip_8.program[offset + 1]);
+        printf("%02x", chip_8.program[offset]);
         printf(" ");
     }
 
     close(fd);
-
-    memcpy(chip_8.memory, FONT, sizeof(FONT));
 }
 
 void emulator_lifecycle_iterate(void *appstate)
 {
+    uint8_t byte_1 = chip_8.memory[chip_8.pc++];
+    uint8_t byte_2 = chip_8.memory[chip_8.pc++];
+    uint16_t opcode = (byte_2 << 8) | byte_1;
+
+    switch (opcode)
+    {
+        case CHIP_8_OPCODE_CLEAR: {
+            return;
+        }
+
+        case CHIP_8_OPCODE_RET: {
+            chip_8.pc = chip_8.stack[--chip_8.sp];
+            return;
+        }
+    }
+
+    switch ((opcode & 0xF000))
+    {
+        case CHIP_8_OPCODE_JUMP: {
+            uint16_t NNN = opcode & 0x0FFF;
+            chip_8.pc = NNN;
+            break;
+        }
+
+        case CHIP_8_OPCODE_CALL: {
+            uint16_t NNN = opcode & 0x0FFF;
+            chip_8.stack[chip_8.sp++] = chip_8.pc;
+            chip_8.pc = NNN;
+            break;
+        }
+
+        case CHIP_8_OPCODE_EQNN: {
+            uint8_t x = byte_2 & 0x0F;
+            if (chip_8.rg[x] == byte_1)
+            {
+                chip_8.pc += 2;
+            }
+            break;
+        }
+
+        case CHIP_8_OPCODE_NENN: {
+            uint8_t x = byte_2 & 0x0F;
+            if (chip_8.rg[x] != byte_1) chip_8.pc += 2;
+            break;
+        }
+
+        case CHIP_8_OPCODE_EQXY: {
+            uint8_t x = byte_2 & 0x0F;
+            uint8_t y = byte_1 >> 4;
+            if (chip_8.rg[x] == chip_8.rg[y]) chip_8.pc += 2;
+            break;
+        }
+
+        case CHIP_8_OPCODE_MOVX: {
+            uint8_t x = byte_2 & 0x0F;
+            chip_8.rg[x] = byte_1;
+            break;
+        }
+
+        case CHIP_8_OPCODE_INCX: {
+            uint8_t x = byte_2 & 0x0F;
+            chip_8.rg[x] += byte_1;
+            break;
+        }
+
+        case CHIP_8_OPCODE_MOVR: {
+            uint8_t x = byte_2 & 0x0F;
+            uint8_t y = byte_1 >> 4;
+            chip_8.rg[x] = chip_8.rg[y];
+            break;
+        }
+
+        case CHIP_8_OPCODE_MOVI: {
+            chip_8.I = opcode & 0x0FFF;
+            break;
+        }
+
+        case CHIP_8_OPCODE_DISP: {
+            uint8_t x = byte_2 & 0x0F;
+            uint8_t y = byte_1 >> 4;
+
+            uint8_t xv = chip_8.rg[x];
+            uint8_t yv = chip_8.rg[y];
+            break;
+        }
+    }
+
     emulator_display_clear();
 
     for (uint16_t x = 0; x < RENDER_WIDTH; x++)
